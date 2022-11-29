2d447c4be272b74463de40f94d10388b9d503fe1
ASUS <ASUS@DESKTOP-KQGGKMU>
2022-11-29 09:48:53 +0300

    Добавьте файлы проекта.

---
+++ Lab.12.v6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {3, -2, 3, 2, 1, -4, 3, 6, 8, -7 }; 
+            double s = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                
+                {
+                    for (int j = i; j < array.Length; j++)
+                    {
+                        s += Math.Abs(array[i]);
+                    }
+                    break;
+                }
+
+            }
+            Console.WriteLine("сумма модулей элементов последовательности: " +s); 
+            Console.ReadKey();
+        }
+    }
+}
